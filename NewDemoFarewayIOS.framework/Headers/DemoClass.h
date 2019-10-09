e8aeb43801a87aab5b8ec9e1f98a1a01854962b6
IMiMineDigital <48542324+IMiMineDigital@users.noreply.github.com>
2019-10-09 15:29:09 +0530

    commit 12

---
+++ NewDemoFarewayIOS.framework/Headers/DemoClass.h
@@ -0,0 +1,17 @@
+//
+//  DemoClass.h
+//  NewDemoFarewayIOS
+//
+//  Created by kamlesh prajapati on 09/10/19.
+//  Copyright © 2019 Instabug. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DemoClass : NSObject
+-(void)getdata;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NewDemoFarewayIOS.framework/Headers/NewDemoFarewayIOS.h
@@ -0,0 +1,20 @@
+//
+//  NewDemoFarewayIOS.h
+//  NewDemoFarewayIOS
+//
+//  Created by kamlesh prajapati on 09/10/19.
+//  Copyright © 2019 Instabug. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <NewDemoFarewayIOS/DemoClass.h>
+
+//! Project version number for NewDemoFarewayIOS.
+FOUNDATION_EXPORT double NewDemoFarewayIOSVersionNumber;
+
+//! Project version string for NewDemoFarewayIOS.
+FOUNDATION_EXPORT const unsigned char NewDemoFarewayIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewDemoFarewayIOS/PublicHeader.h>
+
+
